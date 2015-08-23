b29539a4fae93f4123676c4ebdde3184e9e4b0f1
fengcj <fcjljy@gmail.com>
2015-08-23 21:30:44 +0800

    add carousel

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,31 +1,11 @@
-/*
+$(function(){
 
-My Custom JS
-============
-
-Author:  Brad Hussey
-Updated: August 2013
-Notes:	 Hand coded for Udemy.com
-
-*/
-
-$(function() {
-	
-	$('#alertMe').click(function(e) {
-		
+	$('#alertMe').click(function(e){
 		e.preventDefault();
-		
+		console.log("here");
+		console.log($('#successAlert'));
 		$('#successAlert').slideDown();
-		
-	});
-	
-	$('a.pop').click(function(e) {
-		e.preventDefault();
-	});
-	
-	$('a.pop').popover();
-	
-	$('[rel="tooltip"]').tooltip();
-	
-	
+	})
+
+
 });
